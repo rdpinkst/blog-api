027cd4d9138232dbf2144ed1d6e58a43bc1efc46
Ryan P <rdpinkstaff@gmail.com>
2023-02-26 17:29:53 -0700

    Added user to PostSchema

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
     postBody: { type: String, required: true },
     publish: { type: String, required: true}
